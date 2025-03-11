cda53ec5f3351de09f7e3b8c3af41d7aeb8c4dcd
Matej Bednár <matejbednar55@gmail.com>
2025-03-11 10:21:56 +0100

    stop casting jsonb atribute as text

--- src/drizzle-eftify/helpers/jsonbSelect.ts
+++ src/drizzle-eftify/helpers/jsonbSelect.ts
@@ -2,7 +2,7 @@ import { AnyColumn, SQL, sql } from 'drizzle-orm'
 
 export function jsonbSelect<TProp extends AnyColumn, TResult>(navProperty: TProp, selector: (value: TProp['_']['data']) => TResult): SQL<TResult> {
 	const propName = selector.toString().split('.').pop() as string;
-	const secondPart = `::jsonb->>'${propName}'`
+	const secondPart = `::jsonb->'${propName}'`
 
 	return sql.join([sql`(${navProperty} #>> '{}')`, sql.raw(secondPart)]).as(propName) as any
 }
